b8b998c37727f96e66300478c2eb0956f81c269f
peupeexpriv <165969926+peupeexpriv@users.noreply.github.com>
2024-04-30 10:18:29 -0300

    Add files via upload

---
+++ raça absoluta alem da consciencia.c
@@ -0,0 +1,222 @@
+#include <stdio.h>
+
+int main() {
+    int aluno12, aluno13, aluno14, aluno15, aluno16, aluno17, aluno18;
+    int aluno21, aluno23, aluno24, aluno25, aluno26, aluno27, aluno28;
+    int aluno31, aluno32, aluno34, aluno35, aluno36, aluno37, aluno38;
+    int aluno41, aluno42, aluno43, aluno45, aluno46, aluno47, aluno48;
+    int aluno51, aluno52, aluno53, aluno54, aluno56, aluno57, aluno58;
+    int aluno61, aluno62, aluno63, aluno64, aluno65, aluno67, aluno68;
+    int aluno71, aluno72, aluno73, aluno74, aluno75, aluno76, aluno78;
+    int aluno81, aluno82, aluno83, aluno84, aluno85, aluno86, aluno87;
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno12);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno13);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno14);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno15);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno16);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno17);
+
+    printf("\nAluno 1, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno18);
+
+
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno21);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno23);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno24);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno25);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno26);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno27);
+
+    printf("\nAluno 2, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno28);
+    
+    
+    
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno31);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno32);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno34);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno35);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno36);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno37);
+
+    printf("\nAluno 3, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno38);
+    
+    
+    
+    
+    
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno41);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno42);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno43);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno45);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno46);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno47);
+
+    printf("\nAluno 4, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno48);
+    
+    
+    
+    
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno51);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno52);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno53);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno54);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno56);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno57);
+
+    printf("\nAluno 5, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno58);
+    
+    
+    
+    
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno61);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno62);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno63);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno64);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno65);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno67);
+
+    printf("\nAluno 6, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno68);
+    
+    
+    
+    
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno71);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno72);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno73);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno74);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno75);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno76);
+
+    printf("\nAluno 7, qual a nota que você dá para o Aluno 8?\n");
+    scanf("%d", &aluno78);
+    
+    
+    
+    
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 1?\n");
+    scanf("%d", &aluno81);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 2?\n");
+    scanf("%d", &aluno82);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 3?\n");
+    scanf("%d", &aluno83);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 4?\n");
+    scanf("%d", &aluno84);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 5?\n");
+    scanf("%d", &aluno85);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 6?\n");
+    scanf("%d", &aluno86);
+
+    printf("\nAluno 8, qual a nota que você dá para o Aluno 7?\n");
+    scanf("%d", &aluno87);
+
+
+    float mediaaluno1 = (aluno21 + aluno31 + aluno41 + aluno51 + aluno61 + aluno71 + aluno81) / 7.0;
+    float mediaaluno2 = (aluno12 + aluno32 + aluno42 + aluno52 + aluno62 + aluno72 + aluno82) / 7.0;
+    float mediaaluno3 = (aluno13 + aluno23 + aluno43 + aluno53 + aluno63 + aluno73 + aluno83) / 7.0;
+    float mediaaluno4 = (aluno14 + aluno24 + aluno34 + aluno54 + aluno64 + aluno74 + aluno84) / 7.0;
+    float mediaaluno5 = (aluno15 + aluno25 + aluno35 + aluno45 + aluno65 + aluno75 + aluno85) / 7.0;
+    float mediaaluno6 = (aluno16 + aluno26 + aluno36 + aluno46 + aluno56 + aluno76 + aluno86) / 7.0;
+    float mediaaluno7 = (aluno17 + aluno27 + aluno37 + aluno47 + aluno57 + aluno67 + aluno87) / 7.0;
+    float mediaaluno8 = (aluno18 + aluno28 + aluno38 + aluno48 + aluno58 + aluno68 + aluno78) / 7.0;
+
+    printf("\nMedia aluno 1 = %.2f\n", mediaaluno1);
+    printf("\nMedia aluno 2 = %.2f\n", mediaaluno2);
+    printf("\nMedia aluno 3 = %.2f\n", mediaaluno3);
+    printf("\nMedia aluno 4 = %.2f\n", mediaaluno4);
+    printf("\nMedia aluno 5 = %.2f\n", mediaaluno5);
+    printf("\nMedia aluno 6 = %.2f\n", mediaaluno6);
+    printf("\nMedia aluno 7 = %.2f\n", mediaaluno7);
+    printf("\nMedia aluno 8 = %.2f\n", mediaaluno8);
+
+    return 0;
+}
+
